b659d345221f1ce57d748a46b2cf264812b8fb2b
Binod <binod.yadav@motorolasolutions.com>
2025-01-06 18:45:29 +0530

    using actual API get the data and display the title and movies poster

--- JS/Framwork/imdb_project/src/components/Movies.js
+++ JS/Framwork/imdb_project/src/components/Movies.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+// https://api.themoviedb.org/3/trending/movie/day?language=en-US
 function Movies() {
-
-  // axios.get('https://api.themoviedb.org/3/movie/550?api_key=d0f309eeda258c4300211f9acdc39321').then((res)=>{
-  //   console.log(res.data);
-  // })
-  axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=d0f309eeda258c4300211f9acdc39321').then((res)=>{
-    console.log(res.data.results);
-  })
-  // https://api.themoviedb.org/3/trending/movie/day?language=en-US
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (function () {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/trending/movie/day?api_key=d0f309eeda258c4300211f9acdc39321"
+        )
+        .then((res) => {
+          setMovies(res.data.results);
+        });
+    })();
+  }, []);
+  console.log(movies);
 
   return (
     <div>
       <div className="text-xl font-bold mb-4 text-center">Trending Movies</div>
-      <div className=" flex space-x-4">
-        <div
-          className="w-[150px] h-[40vh] bg-center bg-cover rounded-xl mb-10 hover: scale-100 hover:scale-110 transition duration-500  ease-in-out"
-          style={{
-            backgroundImage:
-              "url(https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg)",
-          }}
-        >
-          <div className="text-white center text-center font-bold bg-black bg-opacity-50 size-small">
-            Jhon winck
-          </div>
-        </div>
+      <div className=" flex space-x-4 flex-wrap ml-4">
+        {movies.map((movie, index) => {
+          return (
+            <div
+              key={index}
+              className="w-[150px] h-[40vh] bg-center bg-cover rounded-xl mb-10 hover: scale-100 hover:scale-110 transition duration-500  ease-in-out"
+              style={{
+                backgroundImage:
+                  // "url(https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg)",
+                  `url(https://image.tmdb.org/t/p/w500/${movie.poster_path})`,
+              }}
+            >
+              <div className="text-white center text-center font-bold bg-black bg-opacity-50 size-small">
+                {movie.title}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
